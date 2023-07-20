4dfb03734444126d025669ac4ced03fdc67f12cb
RyanD177 <w0477770@campus.nscc.ca>
2023-07-20 05:51:45 -0300

    Pet objects and id

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,7 +1,9 @@
+
 const clientId = "bf4cc403b85342aaaf233cfa5b9246ff";
 const clientSecret = "ZDaYVn3qG4EEm0BGhwyRH1KPHhhst1MR";
-const battlePetUrl = "https://us.api.blizzard.com/data/wow/pet/index";
 const accessTokenUrl = "https://us.battle.net/oauth/token";
+const petIndexUrl = "https://us.api.blizzard.com/data/wow/pet/index";
+let accesstoken;
 
  
      fetch(accessTokenUrl, {
@@ -14,7 +16,38 @@ const accessTokenUrl = "https://us.battle.net/oauth/token";
             "Content-Type": "application/x-www-form-urlencoded",
           },
         }).then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+            let accesstoken = data.access_token;
+            console.log(accesstoken);
+
+            const petIndexParameters =  new URLSearchParams ( {
+                namespace: 'static-us',
+                locale: "en_US",
+                access_token: accesstoken,
+            });
+            return fetch(`${petIndexUrl}?${petIndexParameters.toString()}`)
+    
+        })
+        .then((response) => response.json())
+        .then((petIndexData) => {    
+        
+          for(let i = 0; i < petIndexData.pets.length; i++){ 
+            let petData = petIndexData.pets[i];
+            console.log(petData);
+          
+
+          const petIDParas = new URLSearchParams({ 
+            namespace: 'static-us',
+            locale: 'en-US',
+            access_token: accesstoken,
+          })
+          const petId = petData.id;
+          console.log(petId);
+          }
+        
+        }); 
+
+ 
       
         
 
