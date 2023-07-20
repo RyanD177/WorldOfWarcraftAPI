5a496cd815ac4e29882b8387751e5233d4ed9a82
RyanD177 <w0477770@campus.nscc.ca>
2023-07-20 18:16:13 -0300

    cleaner code

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,58 +1,41 @@
-
 const clientId = "bf4cc403b85342aaaf233cfa5b9246ff";
 const clientSecret = "ZDaYVn3qG4EEm0BGhwyRH1KPHhhst1MR";
 const accessTokenUrl = "https://us.battle.net/oauth/token";
 const petIndexUrl = "https://us.api.blizzard.com/data/wow/pet/index";
 let accesstoken;
 
- 
-     fetch(accessTokenUrl, {
-        method:"POST",
-        body: new URLSearchParams({
-            grant_type: 'client_credentials',
-        }),
-        headers: {
-            Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }).then((response) => response.json())
-        .then((data) => {
-            let accesstoken = data.access_token;
-            console.log(accesstoken);
+async function battlePets() {
+  const response = await fetch(accessTokenUrl, {
+    method: "POST",
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+    }),
+    headers: {
+      Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+  let accesstoken = data.access_token;
+  console.log(accesstoken);
 
-            const petIndexParameters =  new URLSearchParams ( {
-                namespace: 'static-us',
-                locale: "en_US",
-                access_token: accesstoken,
-            });
-            return fetch(`${petIndexUrl}?${petIndexParameters.toString()}`)
-    
-        })
-        .then((response) => response.json())
-        .then((petIndexData) => {    
-        
-          for(let i = 0; i < petIndexData.pets.length; i++){ 
-            let petData = petIndexData.pets[i];
-            console.log(petData);
-          
+ // pet index url search params
 
-          const petIDParas = new URLSearchParams({ 
-            namespace: 'static-us',
-            locale: 'en-US',
-            access_token: accesstoken,
-          })
-          const petId = petData.id;
-          console.log(petId);
-          }
-        
-        }); 
+ const petIndexParameters = new URLSearchParams({
+    namespace: 'static-US',
+    locale: "en-US",
+    access_token: accesstoken,
+ })
 
- 
-      
-        
+ const petIndexResponse = await fetch(`${petIndexUrl}?${petIndexParameters.toString()}`)
+ const petIndexData = await petIndexResponse.json();
+ console.log(petIndexData);
+
+}
 
 
 
 
 
-
+battlePets();
